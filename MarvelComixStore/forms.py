5d4fa9f956149157852174224f602be45b899d73
igor.malinov <miltrix@yandex.ru>
2017-09-27 14:25:16 +0300

    Error cant create migrations

--- MarvelComixStore/forms.py
+++ MarvelComixStore/forms.py
@@ -1,14 +1,23 @@
 from django.forms import Form
 from django import forms
-from django.utils import timezone
-from MarvelComixStore.models import Comix,Tag
+
+from MarvelComixStore.models import Comix, Tag
+
 
 class searchForm(Form):
     keywords = forms.CharField(max_length=100, label="Ключевые слова или тэги", required=False)
-    iquery = []
-    for i in Comix.objects.all():
-        iquery.append(i.getYear())
-    iset=set(iquery)
-    iquery_choices = [(0, '----------')] + [(i, i) for i in iset]
-    year=forms.ChoiceField(iquery_choices, label='Год выпуска',required=False)
-    iquery=Tag.objects.values_list('name',flat=True).distinct()
+    year = forms.ChoiceField([], label='Год выпуска', required=False)
+    iquery = Tag.objects.values_list('name', flat=True).distinct()
+
+    def __init__(self, *args, **kwargs):
+        super(searchForm, self).__init__(*args, **kwargs)
+
+        self.fields['year'].choices = self.populate_years()
+
+    def populate_years(self):
+        iquery = []
+        for i in Comix.objects.all():
+            iquery.append(i.getYear())
+        iset = set(iquery)
+        iquery_choices = [(0, '----------')] + [(i, i) for i in iset]
+        return iquery_choices
